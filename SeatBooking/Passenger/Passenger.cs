a72474111122b5f0f64bf8694b020aa5445c8b15
Kabitha <kabi.kabila.171@gmail.com>
2024-10-03 17:48:58 +0530

    ADDED:  ProjectFiles

---
+++ SeatBooking/Passenger/Passenger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FlightSeatBooking
+{
+    public class Passenger
+    {
+        public string strName { get; set; }
+
+        public string strGender { get; set; }
+
+        public int nAge { get; set; }
+
+        public Passenger(string strName, string strGender, int nAge)
+        {
+            this.strName = strName;
+            this.strGender = strGender;
+            this.nAge = nAge;
+        }
+    }
+  }
+
---
+++ SeatBooking/SeatAvailable/SeatAvailable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlightSeatBooking
+{
+    public class SeatAvailable
+    {
+        public bool IsBooked { get; set; }
+
+        public Passenger passenger { get; set; }
+
+    }
+}
