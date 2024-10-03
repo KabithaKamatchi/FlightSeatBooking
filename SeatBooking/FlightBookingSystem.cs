d89a97cf365dbc859dd7ab66f749a6be97789393
Kabitha <kabi.kabila.171@gmail.com>
2024-10-03 13:19:47 +0530

    Add project files.

---
+++ SeatBooking/FlightBookingSystem.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace FlightBookingSystem
+{
+    public class Passenger
+    {
+        public string strName { get; set; }
+
+        public string strGender { get; set; }
+
+        public int nAge { get; set; }
+
+        public Passenger(string strName, string strGender, int nAge)
+        {
+            this.strName = strName;
+            this.strGender = strGender;
+            this.nAge = nAge;
+        }
+    }
+
+    public class SeatAvailable
+    {
+        public bool IsBooked { get; set; }
+
+        public Passenger passenger { get; set; }
+
+        public SeatAvailable()
+        {
+            IsBooked = false;
+        }
+    }
+
+    public class FlightBooking
+    {
+        SeatAvailable[][] seats;
+        char[] cSeatLabels = { 'A', 'B', 'C', 'D', 'E' };
+        int nRows = 10 ;
+        int nSeatsPerRow = 5;
+        public FlightBooking(int nRows , int nSeatsPerRow )
+        {
+            this.nRows = nRows;
+            this.nSeatsPerRow = nSeatsPerRow;
+            seats = new SeatAvailable[nRows][];
+
+            for (int i = 0; i < nRows; i++)
+            {
+                seats[i] = new SeatAvailable[nSeatsPerRow];
+                for (int j = 0; j < nSeatsPerRow; j++)
+                {
+                    seats[i][j] = new SeatAvailable();
+                }
+            }
+        }
+
+        public bool BookSeat(int nSeatRow, int nSeatNumber, Passenger passenger)
+        {
+            if (passenger == null)
+            {
+                Console.WriteLine("Error:Passenger cannot be null");
+                return false;
+            }
+
+            int nCheckingRowIndex = nSeatRow - 1;
+            int nCheckingColumnIndex = nSeatNumber - 1;
+
+            if (seats[nCheckingRowIndex][nCheckingColumnIndex].IsBooked)
+            {
+                Console.WriteLine($"Seat{nSeatRow} - {cSeatLabels[nCheckingColumnIndex]} is already Booked");
+                return false;
+            }
+            else
+            {
+                seats[nCheckingRowIndex][nCheckingColumnIndex].IsBooked = true;
+                seats[nCheckingRowIndex][nCheckingColumnIndex].passenger = passenger;
+                Console.WriteLine($"Seat {nSeatRow} - {nSeatNumber} booked successfully for {passenger.strName}");
+                return true;
+            }
+        }
+
+        public void ShowAvailableSeatNumber()
+        {
+            Console.WriteLine("Available Seats: ");
+            for (int i = 0; i < nRows; i++)
+            {
+                for (int j = 0; j < nSeatsPerRow; j++)
+                {
+                    if (!seats[i][j].IsBooked)
+                    {
+                        Console.Write($"[{i + 1} {cSeatLabels[j]}");
+                    }
+                    Console.WriteLine(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public bool IsFullyBooked()
+        {
+            for (int i = 0; i < seats.Length; i++)
+            {
+                for (int j = 0; j <= seats[i].Length; j++)
+                {
+                    if (!seats[i][j].IsBooked)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
+
+
+      
+ 
+
+
+
+  
+
+
+       
+
---
+++ SeatBooking/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace FlightBookingSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            FlightBooking flightBooking = new FlightBooking(5, 5);
+
+            while (true)
+            {
+                flightBooking.ShowAvailableSeatNumber();
+
+                if (flightBooking.IsFullyBooked())
+                {
+                    Console.WriteLine("The flight is fully booked.");
+                    break;
+                }
+
+                Console.WriteLine("Enter passenger name: ");
+                string passengerName = Console.ReadLine();
+
+                Console.WriteLine("Enter passenger gender (Male/Female): ");
+                string passengerGender = Console.ReadLine();
+
+                Console.WriteLine("Enter passenger age: ");
+                if (!int.TryParse(Console.ReadLine(), out int passengerAge))
+                {
+                    Console.WriteLine("Invalid age entered. Please enter a number.");
+                    continue;
+                }
+
+                Console.WriteLine("Enter seat row number: ");
+                if (!int.TryParse(Console.ReadLine(), out int seatRow) || seatRow < 1 || seatRow > 5)
+                {
+                    Console.WriteLine("Invalid row number. Please enter a number between 1 and 5.");
+                    continue;
+                }
+
+                Console.WriteLine("Enter seat column number : ");
+                if (!int.TryParse(Console.ReadLine(), out int seatColumn) || seatColumn < 1 || seatColumn > 5 )
+                {
+                    Console.WriteLine("Invalid column number. Please enter a number between 1 and 10.");
+                    continue;
+                }
+
+                Passenger passenger = new Passenger(passengerName, passengerGender, passengerAge);
+                flightBooking.BookSeat(seatRow, seatColumn, passenger);
+            }
+        }
+    }
+    }
+
+        
